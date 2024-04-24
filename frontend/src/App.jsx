c7d68f5a366970e51c498a8b9eef7eca713a9c50
MOHDZAMA <mohdzama92@gmail.com>
2024-04-24 18:25:55 +0530

    fix routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,30 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Homee from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
-
 function App() {
-  const {authUser} = useAuthContext();
+  const { authUser } = useAuthContext();
+  console.log(authUser);
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
+    <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-                <Route path='/' element={ <Homee /> } />
-                <Route path='/login' element={ <Login />} />
-          { <Route path='/signup' element={ <SignUp />} /> } 
-        <Route path='/signup' element={authUser ?  <Navigate to='/' /> : <SignUp /> } />
-            </Routes>
+        {authUser ? (
+          <>
+            <Route path="/" element={<Home />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </>
+        )}
+      </Routes>
     </div>
-  )
+  );
 }
+
 export default App;
